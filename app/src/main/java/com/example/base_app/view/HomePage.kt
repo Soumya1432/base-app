2ca35c17d07905977c75db0ae5e0c4c4dcfcefb8
Soumya Biswas <biswassoumya17@gmail.com>
2025-08-06 12:45:03 +0530

    implement loading features

--- app/src/main/java/com/example/base_app/view/HomePage.kt
+++ app/src/main/java/com/example/base_app/view/HomePage.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
@@ -15,6 +16,7 @@ import com.example.base_app.viewmodel.HomeViewModel
 fun HomePage(modifier: Modifier = Modifier,viewModel: HomeViewModel) {
 
     val userData  = viewModel.userData.observeAsState()
+    val isLoading = viewModel.isLoading.observeAsState()
 
 
     Column(modifier= Modifier.fillMaxSize(),
@@ -28,9 +30,22 @@ fun HomePage(modifier: Modifier = Modifier,viewModel: HomeViewModel) {
             Text(text = "get Data")
         }
 
-        userData.value?.name?.let {
-            Text(text = "NAME $it")
+        if(isLoading.value==true){
+            CircularProgressIndicator()
         }
+        else
+        {
+            userData.value?.name?.let {
+                Text(text = "NAME $it")
+            }
+            userData.value?.age?.let {
+                Text(text = "AGE $it")
+            }
+        }
+
+
+
+
 
     }
 }
--- app/src/main/java/com/example/base_app/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/base_app/viewmodel/HomeViewModel.kt
@@ -13,11 +13,15 @@ class HomeViewModel: ViewModel() {
     val userRepository : UserRepository = UserRepository()
     private  val _userData = MutableLiveData<UserData>()
     val userData : LiveData<UserData> = _userData
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading : LiveData<Boolean> = _isLoading
+    
     fun getUserData(){
-
+        _isLoading.postValue(true)
         viewModelScope.launch {
             val userResult = userRepository.fetchUserData()
             _userData.postValue(userResult)
+            _isLoading.postValue(false)
         }
 
 
